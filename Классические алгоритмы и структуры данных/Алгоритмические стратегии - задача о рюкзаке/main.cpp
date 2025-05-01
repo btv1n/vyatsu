16569e688f8642c8cb6d19184a862ef67d9af976
btv1n <jammix700@gmail.com>
2025-05-01 14:33:22 +0300

    Add files via upload

---
+++ Классические алгоритмы и структуры данных/Алгоритмические стратегии - задача о рюкзаке/main.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+/*
+Условие задачи:
+
+Алгоритмические стратегии
+Реализовать задачу о рюкзаке жадным алгоритмом (нахождение оптимального решения не гарантируется).
+В этой версии задачи о рюкзаке мы можем разбивать объекты на части (дробная задача о рюкзаке).
+*/
+
+
+/* 
+Условие, когда нельзя делить предметы:
+
+		if (arr[i].weight <= W) // если элемент полностью влезает в рюкзак, добавляем его целиком
+		{
+			W -= arr[i].weight;
+			result_value += arr[i].value;
+		}
+		else
+		{
+			continue;
+		}
+*/
+
+
+/*
+Функция sort в данном коде используется для сортировки массива элементов Item по соотношению значение/вес.
+в функции Knapsack вызывается функция sort, которая принимает в качестве параметров указатели на начало и 
+конец массива arr, а также указатель на функцию Comparison. Эта функция сортирует элементы массива arr в порядке 
+убывания соотношения значение/вес.
+*/
+
+
+// Структура элемента, в которой хранится вес и значение элемента
+struct Item
+{
+	int value, weight;
+
+	Item(int value, int weight)
+	{
+		this->value = value;
+		this->weight = weight;
+	}
+};
+
+// Функция сравнения для сортировки элементов по соотношению значение/вес
+bool Comparison(struct Item a, struct Item b)
+{
+	double number_1 = (double)a.value / (double)a.weight;
+	double number_2 = (double)b.value / (double)b.weight;
+	return number_1 > number_2; // true | false
+}
+
+// Жадный алгоритм
+double Knapsack(int W, struct Item arr[], int N)
+{
+	// Сортировка элементов по соотношению
+	sort(arr, arr + N, Comparison); // сортируем весь массив (от и до), используя сравнение
+
+	double result_value = 0.0;
+
+	// Перебор всех элементов
+	for (int i = 0; i < N; i++)
+	{
+		if (arr[i].weight <= W) // если элемент полностью влезает в рюкзак, добавляем его целиком
+		{
+			W -= arr[i].weight;
+			result_value += arr[i].value;
+		}
+		else // если элемент не влезает в рюкзак целиком, то добавляем его дробную часть
+		{
+			// continue; // если нельзя разбивать предметы на части
+			result_value += arr[i].value * ((double)W / (double)arr[i].weight);
+			break;
+		}
+	}
+
+	return result_value;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int W = 5; // вместимость рюкзака
+
+	// объекты = значение/вес
+	Item arr[] =
+	{
+		//{ 12, 3},
+		{ 20, 1 }, // 20 / 1 = 20
+		{ 38, 2 }, // 38 / 2 = 19
+		{ 10, 3 },
+		{ 29, 4 }
+	};
+
+	int N = sizeof(arr) / sizeof(arr[0]); // кол-во элементов в массиве
+
+	std::cout << "Максимально возможное значение: " << Knapsack(W, arr, N);
+
+	return 0;
+}
---
+++ Классические алгоритмы и структуры данных/Перебор с возвратом - задача о шахматах/main.cpp
@@ -0,0 +1,137 @@
+﻿#include <iostream>
+#include <vector>
+#include "math.h"
+
+using namespace std;
+
+/*
+Условие задачи:
+
+Перебор с возвратом
+По заданным числам n и k определить все способы, которыми можно расставить k слонов
+на шахматной доске размера n×n так, чтобы никакие два из них не били друг друга.
+*/
+
+
+bool check(int n, int row, int col, int** board)
+{
+    // Проверка на атаку по левой верхней диагонали
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+    {
+        if (board[i][j] == 1)
+        {
+            return false;
+        }
+    }
+
+    // Проверка на атаку по левой нижней диагонали
+    for (int i = row, j = col; i < n && j >= 0; i++, j--)
+    {
+        if (board[i][j] == 1)
+        {
+            return false;
+        }
+    }
+
+    // Проверка на атаку по правой верхней диагонали
+    for (int i = row, j = col; i >= 0 && j < n; i--, j++)
+    {
+        if (board[i][j] == 1)
+        {
+            return false;
+        }
+    }
+
+    // Проверка на атаку по правой нижней диагонали
+    for (int i = row, j = col; i < n && j < n; i++, j++)
+    {
+        if (board[i][j] == 1)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void solve(int n, int k, int row, int col, int count, int& countPosition, int** board)
+{
+    // Если разместили k слонов, выводим доску
+    if (count == k)
+    {
+        countPosition += 1; // Увеличиваем счетчик расстановок на единицу
+
+        //// Выводит доску с расстановкой слонов
+        //for (int i = 0; i < n; i++)
+        //{
+        //    for (int j = 0; j < n; j++)
+        //    {
+        //        cout << board[i][j] << " ";
+        //    }
+        //    cout << endl;
+        //}
+        //cout << endl;
+
+        return;
+    }
+
+    // Перебираем все возможные позиции для слона
+    for (int i = row; i < n; i++) // Цикл по строкам пока не достигнем последней строки
+    {
+        for (int j = col; j < n; j++) // Цикл по столбцам пока не достигнем последнего столбца
+        {
+            if (check(n, i, j, board)) // Проверяем можно ли поставить слона в это поле т.е. не находится ли поле под боем других слонов
+            {
+                board[i][j] = 1; // Размещаем слона
+
+                solve(n, k, i, j, count + 1, countPosition, board); // Рекурсивно вызываем функцию
+
+                board[i][j] = 0; // Убираем слона (возврат)
+            }
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int countPosition = 0; // Счетчик количества всех возможных расстановок
+    int n, k;
+
+    cout << "Введите сначала n, а потом k: ";
+    cin >> n >> k;
+
+    // Создание двумерного поля
+    int** board = new int* [n];
+    for (int i = 0; i < n; i++)
+    {
+        board[i] = new int[n];
+    }
+
+    // Заполнение всех клеток поля нулями
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            board[i][j] = 0;
+        }
+    }
+
+    // row, col - определяют позицию начала перебора ; count - счетчик поставленных фигур
+    int row = 0, col = 0, count = 0;
+
+    solve(n, k, row, col, count, countPosition, board); // Вызов функции для размещения слонов
+
+    cout << "Количество возможных расстановок: " << countPosition;
+
+
+    // Очистка памяти, удаление массива
+    for (int i = 0; i < n; i++)
+    {
+        delete[] board[i];
+    }
+    delete[] board;
+
+    return 0;
+}

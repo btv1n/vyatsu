efaec13c6243f8e6d79a45361d0098c945899aeb
btv1n <jammix700@gmail.com>
2025-05-03 21:02:16 +0300

    Add files via upload

---
+++ Программирование на C#/Лабораторная работа №4/Lab_4/Task_8/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Task_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(75, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вы довольны";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(119, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " своей";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(88, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "зарплатой?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ДА";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(252, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "НЕТ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 330);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Вопрос работодателя";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Программирование на C#/Лабораторная работа №4/Lab_4/Task_8/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+4.8.Напишите приложение «Вопрос работодателя», которое выполняет следующие действия:
+–	первоначально форма имеет следующий вид (рис. 4.8);
+–	кнопка «Да» имеет подсказку «Нажми сюда». При нажатии на кнопку «Да» приложение 
+закрывается, выводя сообщение «Мы так и думали!»;
+–	при попытке нажатия на кнопку «Нет» эта кнопка перемещается по форме в произвольном 
+направлении, не уходя за границы формы (рис. 4.9).
+Указания к решению: для отображения всплывающей подсказки воспользуйтесь компонентом 
+ToolTip и свойством ToolTip on toolTip1 у соответствующего компонента Button.
+*/
+
+
+namespace Task_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ToolTip toolTip = new ToolTip();
+            toolTip.SetToolTip(button1, "Нажми сюда");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Мы так и думали!");
+            this.Close();
+
+            //Form2 window = new Form2();
+            //window.Show();
+            //this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            // Генерация случайных координат для кнопки
+            int newX = random.Next(0, ClientSize.Width - button2.Width);
+            int newY = random.Next(0, ClientSize.Height - button2.Height);
+
+            // Перемещение кнопки на новые координаты
+            button2.Location = new Point(newX, newY);
+        }
+    }
+}

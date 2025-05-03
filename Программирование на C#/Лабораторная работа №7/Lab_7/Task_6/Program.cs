158609d77bac38ff56be4abb2f05a9d91d7925b2
btv1n <jammix700@gmail.com>
2025-05-03 20:52:12 +0300

    Add files via upload

---
+++ Программирование на C#/Лабораторная работа №7/Lab_7/Task_6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+7.6.Напишите программу, которая для заданного каталога создает подкаталоги, соответствующие дате создания каждого 
+отдельного файла, и перемещает каждый файл в соответствующий дате каталог.
+*/
+
+namespace Task_6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string directoryPath = "C:\\for_task6";
+            string dateDirectory;
+            string destinationFile;
+
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+
+            
+            if (!directory.Exists) // проверка существования указанного каталога
+            {
+                Console.WriteLine("Указанный каталог не существует.");
+            }
+            else
+            {
+                foreach (FileInfo file in directory.GetFiles()) // перебор всех файлов в указанном каталоге
+                {
+                    // Задание пути каталога с датой создания файла
+                    dateDirectory = Path.Combine(directoryPath, file.CreationTime.ToString("dd-MM-yyyy"));
+
+                    // Создание директории с указанным путем
+                    Directory.CreateDirectory(dateDirectory);
+
+                    // Путь, который указывает, куда нужно перенести файл
+                    destinationFile = Path.Combine(dateDirectory, file.Name);
+
+                    // Перемещение файла в указанную директорию
+                    file.MoveTo(destinationFile);
+                }
+
+                Console.WriteLine("Файлы успешно перенесены.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Программирование на C#/Лабораторная работа №7/Lab_7/Task_7/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+7.7.Напишите программу, которая удаляет все подкаталоги, вложенные в заданный.
+*/
+
+/*
+                try
+                {
+                    
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("Каталог не пуст");
+                }
+*/
+
+
+namespace Task_7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Заданный путь к каталогу
+            string path = "C:\\for_task";
+
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            // Проверка существования каталога
+            if (directory.Exists)
+            { 
+                DeleteSubdirectories(directory); // удаление всех подкаталогов
+                Console.WriteLine("Все подкаталоги удалены.");
+            }
+            else
+            {
+                Console.WriteLine("Указанный каталог не существует.");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void DeleteSubdirectories(DirectoryInfo directory)
+        {
+            string[] files;
+
+            foreach (var subdirectory in directory.GetDirectories()) // удаление всех подкаталогов
+            {
+                DeleteSubdirectories(subdirectory); // рекурсивный вызов для удаления подкаталогов внутри
+
+                files = Directory.GetFiles("C:\\for_task\\" + subdirectory.Name); // путь к файлам в подкаталоге
+
+                foreach (string file in files) // перебираем файлы в подкаталоге и удаляем их
+                {
+                    File.Delete(file);
+                }
+
+                subdirectory.Delete(); // удаление текущего подкаталога
+            }
+        }
+    }
+}

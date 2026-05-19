b894758000424d7ddbf80dca7ed76b1437b72646
btv1n <jammix700@gmail.com>
2026-05-19 13:07:04 +0300

    Delete Объектно-ориентированное программирование/Код directory

--- Объектно-ориентированное программирование/Код/Lecture2/Task1/Point.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct Point
-{
-	double x;
-	double y;
-};
-
-double distance(Point, Point);
--- Объектно-ориентированное программирование/Код/Lecture2/Task1/Segment.cpp
+++
@@ -1,6 +0,0 @@
-#include "Segment.h"
-
-double distance(Segment s)
-{
-    return sqrt(pow((s.first.x - s.last.x), 2) + pow((s.first.y - s.last.y), 2));
-}
